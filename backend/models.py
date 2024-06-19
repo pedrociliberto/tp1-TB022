e1521a76ba0dd2e0c40fa1e04147305d950dc667
Pedro Ciliberto <pciliberto@fi.uba.ar>
2024-06-18 22:23:41 -0300

    Add 'genero', 'pais_origen' & 'imagen' columns on 'Banda' table model

---
+++ backend/models.py
@@ -0,0 +1,22 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Banda(db.Model):
+    __tablename__ = 'bandas'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(30), nullable=False)
+    anio_creacion = db.Column(db.Integer)
+    genero = db.Column(db.String(30))
+    pais_origen = db.Column(db.String(30))
+    imagen = db.Column(db.String(200))
+
+class Album(db.Model):
+    __tablename__ = 'albums'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(30), nullable=False)
+    anio_publicado = db.Column(db.Integer)
+    banda_id = db.Column(db.Integer, db.ForeignKey('bandas.id'), nullable=False)
+    imagen = db.Column(db.String(200))
