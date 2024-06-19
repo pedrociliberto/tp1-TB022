891cb48368c03bf0ae8104c4f9f96f38ba4bfcdb
Pedro Ciliberto <pciliberto@fi.uba.ar>
2024-06-18 22:32:22 -0300

    Add 'Bandas' JavaScript dynamics file, including request to backend server for the usage of all bands

---
+++ frontend/bandas/dynamics-bandas.js
@@ -0,0 +1,42 @@
+function handle_response (response) {
+    return response.json()
+}
+
+function parse_bandas (bandas) {
+    console.log("TENGO BANDAS")
+    
+    for (let index = 0; index < bandas.length; index++) {
+        
+        container = document.getElementById("container");
+
+        const banda = document.createElement("div");
+        banda.setAttribute("class", "banda");
+
+        const card = document.createElement("a");
+        card.setAttribute("class", "card");
+        card.setAttribute("href", `/bandas/${bandas[index].id}`);
+
+        const image = document.createElement("img");
+        image.setAttribute("class", "band-image");
+        image.setAttribute("src", bandas[index].imagen);
+
+        const name = document.createElement("h3");
+        name.innerHTML = bandas[index].nombre;
+
+        card.append(image);
+        card.append(name);
+
+        banda.append(card);
+
+        container.append(banda);
+    }
+}
+
+function handle_error (error) {
+    console.log("ERROR", error)
+}
+
+fetch("http://localhost:5000/bandas/")
+    .then(handle_response)
+    .then(parse_bandas)
+    .catch(handle_error)
