d9ce9c8302d2a7b66fa5c17d4faa26b512425c1b
Pedro Ciliberto <pciliberto@fi.uba.ar>
2024-06-20 00:04:34 -0300

    Add 'bandas/<id_banda>' GET endpoint, 'bandas/<id_banda>' DELETE endpoint, and 'bandas/' PUT endpoint

--- backend/app.py
+++ backend/app.py
@@ -34,11 +34,61 @@ def obtener_bandas():
                 'pais_origen': pais_origen,
                 'imagen': imagen
             })
-        
+
         return jsonify(tabla_bandas)
     except:
         return jsonify({'message': 'Error al obtener las bandas'}), 500
     
+@app.route("/bandas/<id_banda>", methods = ['GET'])
+def obtener_banda(id_banda):
+    try:
+        banda = Banda.query.where(Banda.id == id_banda).first()
+
+        banda_data = {
+            'id': banda.id,
+            'nombre': banda.nombre,
+            'anio_creacion': banda.anio_creacion,
+            'genero': banda.genero,
+            'pais_origen': banda.pais_origen,
+            'imagen': banda.imagen
+        }
+        return banda_data
+    except:
+        return jsonify({'message': 'Error al obtener la banda'}), 500
+    
+@app.route("/bandas/", methods = ['PUT'])
+def actualizar_banda():
+    id = request.json['id']
+    nombre = request.json['nombre']
+    genero = request.json['genero']
+    anio_creacion = request.json['anio_creacion']
+    pais_origen = request.json['pais_origen']
+    imagen = request.json['imagen']
+
+    banda_actualizada = {
+        'id': id,
+        'nombre': nombre,
+        'genero': genero,
+        'anio_creacion': anio_creacion,
+        'pais_origen': pais_origen,
+        'imagen': imagen
+    }
+
+    try:
+        Banda.query.filter_by(id = id).update(banda_actualizada)
+        db.session.commit()
+        return jsonify({'success': True})
+    except:
+        return jsonify({'success': False}), 500
+
+@app.route("/bandas/<id_banda>", methods = ['DELETE'])
+def eliminar_banda(id_banda):
+    try:
+        Banda.query.filter_by(id = id_banda).delete()
+        db.session.commit()
+        return jsonify({'success': True})
+    except:
+        return jsonify({'success': False}), 500
 
 if __name__ == '__main__':
     print('Iniciando servidor en http://localhost:5000')
