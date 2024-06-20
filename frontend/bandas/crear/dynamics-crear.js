47d96278af4240da4abcbe064563939c698255aa
Pedro Ciliberto <pciliberto@fi.uba.ar>
2024-06-20 01:16:52 -0300

    Finish JavaScript dynamics, including the following endpoints:
    - '/bandas/count' - GET | Petition that requests amount of bands already created
    - '/bandas/'     - POST | Petition to create new band, based on user's form details

---
+++ frontend/bandas/crear/dynamics-crear.js
@@ -0,0 +1,53 @@
+function create_band(data) {
+    if (data.success === true) {
+        alert("Banda creada correctamente")
+        window.location.href = `/bandas/banda_ind?id=${data.id}`
+    } else {
+        alert("Error al crear banda")
+    }
+}
+
+function handle_error(error) {
+    console.log("ERROR", error)
+}
+
+function crear_banda(event) {
+    event.preventDefault()
+
+    const form = new FormData(event.target)
+
+    const nombre = form.get("nombre")
+    const genero = form.get("genero")
+    const anio_creacion = form.get("anio_creacion")
+    const pais_origen = form.get("pais_origen")
+    const imagen = form.get("imagen")
+    
+    function count_bands(data) {
+        const count = data.cantidad_bandas
+
+        const id = count + 1
+
+        fetch("http://localhost:5000/bandas/", {
+        method: "POST",
+        body: JSON.stringify({
+            id: id,
+            nombre: nombre,
+            genero: genero,
+            anio_creacion: anio_creacion,
+            pais_origen: pais_origen,
+            imagen: imagen
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then(response => response.json())
+        .then(create_band)
+        .catch(handle_error)
+    }
+    
+    fetch("http://localhost:5000/bandas/count")
+        .then(response => response.json())
+        .then(count_bands)
+        .catch(handle_error)
+}
