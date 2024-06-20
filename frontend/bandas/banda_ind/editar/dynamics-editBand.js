2a395ca4c3c1c3a36e8d71e7283592e9f5e53cdb
Pedro Ciliberto <pciliberto@fi.uba.ar>
2024-06-20 00:17:25 -0300

    Add JavaScript dynamics for individual bands' edit page, including a GET request to a given id, and a PUT petition requesting changes in a band's details

---
+++ frontend/bandas/banda_ind/editar/dynamics-editBand.js
@@ -0,0 +1,65 @@
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+
+if (id === null || id === "" || id === undefined || isNaN(id) || id < 0 || id % 1 !== 0) {
+    window.location.href = "/";
+}
+
+function parse_data(data) {
+    document.getElementById("nombre").value = data.nombre
+    document.getElementById("genero").value = data.genero
+    document.getElementById("anio_creacion").value = data.anio_creacion
+    document.getElementById("pais_origen").value = data.pais_origen
+    document.getElementById("imagen").value = data.imagen
+}
+
+function handle_error(error) {
+    console.log("ERROR", error)
+}
+
+fetch(`http://localhost:5000/bandas/${id}`)
+    .then((response) => response.json())
+    .then(parse_data)
+    .catch(handle_error)
+
+
+function update_data(data) {
+    if (data.success === true) {
+        alert("Banda editada exitosamente")
+        window.location.href = `/bandas/banda_ind?id=${id}`
+    } else {
+        alert("No se pudo editar la banda")
+    }
+}
+
+function editar_banda(event) {
+    event.preventDefault()
+
+    const form = new FormData(event.target)
+
+    const nombre = form.get("nombre")
+    const genero = form.get("genero")
+    const anio_creacion = form.get("anio_creacion")
+    const pais_origen = form.get("pais_origen")
+    const imagen = form.get("imagen")
+
+    fetch(`http://localhost:5000/bandas/`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            id: id,
+            nombre: nombre,
+            genero: genero,
+            anio_creacion: anio_creacion,
+            pais_origen: pais_origen,
+            imagen: imagen
+        })
+    })
+        .then((response) => response.json())
+        .then(update_data)
+        .catch(handle_error)
+        
+    
+}
