da25de1f0356d8082633f91cd6938c0ce5957227
Pedro Ciliberto <pciliberto@fi.uba.ar>
2024-06-23 19:54:49 -0300

    Move band albums dynamics to this file

--- frontend/bandas/banda_ind/dynamics-banda-ind.js
+++ frontend/bandas/banda_ind/dynamics-banda-ind.js
@@ -23,8 +23,6 @@ function parse_banda (banda) {
     image.setAttribute("src", banda.imagen)
     image.setAttribute("alt", banda.nombre)
 
-    const descripcion = document.getElementById("description")
-
     const anio_creacion = document.getElementById("anio_creacion")
     anio_creacion.innerHTML = banda.anio_creacion
 
@@ -72,3 +70,42 @@ function eliminar_banda () {
         .then(handle_delete)
         .catch(handle_error)
 }
+
+/* ------------------------------------------- */
+
+function parse_albums (albums) {
+    console.log("TENGO ALBUMS")
+    
+    for (let index = 0; index < albums.length; index++) {
+        
+        container = document.getElementById("container-albums");
+
+        const description = document.getElementById("description");
+
+        const album = document.createElement("div");
+        album.setAttribute("class", "album");
+
+        const card = document.createElement("a");
+        card.setAttribute("class", "card");
+        card.setAttribute("href", `/albums/album_ind?id=${albums[index].id}`);
+
+        const image = document.createElement("img");
+        image.setAttribute("class", "album-image");
+        image.setAttribute("src", albums[index].imagen);
+
+        const name = document.createElement("h3");
+        name.innerHTML = albums[index].nombre;
+
+        card.append(image);
+        card.append(name);
+
+        album.append(card);
+
+        container.append(album);
+    }
+}
+
+fetch(`http://localhost:5000/albums/banda/${id}`)
+    .then((res) => res.json())
+    .then(parse_albums)
+    .catch(handle_error)
